837ba62fda0e195313a32704cea357608925b412
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2024-11-03 01:23:09 +0600

    save

--- src/xorf.zig
+++ src/xorf.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const rotl = std.math.rotl;
+const math = std.math;
+const rotl = math.rotl;
 const Allocator = std.mem.Allocator;
 const SplitMix64 = std.Random.SplitMix64;
 
@@ -9,10 +10,6 @@ fn make_fingerprint(comptime Fingerprint: type, hash: u64) Fingerprint {
 
 fn reduce(len: u32, x: u32) u32 {
     return @truncate((@as(u64, len) * @as(u64, x)) >> 32);
-}
-
-fn reduce64(len: 64, x: 64) 64 {
-    return @truncate((@as(u128, len) * @as(u128, x)) >> 64);
 }
 
 pub const Header = struct {
@@ -26,12 +23,13 @@ pub fn prepare_subhashes(comptime arity: comptime_int, header: *const Header, ha
 }
 
 fn make_subhashes(comptime arity: comptime_int, header: *const Header, h: u64) [arity]u32 {
-    const hl = reduce64(@as(u64, header.segment_count_len), h);
+    const hl = reduce(header.segment_count_len, @truncate(h));
 
     var subhashes: [arity]u32 = undefined;
     comptime var rot = 0;
     inline for (0..arity) |i| {
-        subhashes[i] = (hl + i * @as(u64, header.segment_len)) ^ (rotl(u64, h, rot) & (header.segment_len - 1));
+        const rotated: u32 = @truncate(rotl(u64, h, rot));
+        subhashes[i] = (hl + @as(u32, @truncate(i)) * header.segment_len) ^ (rotated & (header.segment_len - 1));
         rot += 64 / arity;
     }
     return subhashes;
@@ -63,8 +61,51 @@ pub const ConstructError = error{
     ConstructFail,
 };
 
+fn calculate_segment_length_impl(size: u32, logdiv: f64, diff: f64) u32 {
+    const float_size: f64 = @floatFromInt(size);
+    const logres = @log(float_size) / @log(logdiv) + diff;
+    const floored = @floor(logres);
+    const int_val: u32 = @intFromFloat(floored);
+    return math.shl(u32, 1, int_val);
+}
+
+fn calculate_segment_length(comptime arity: comptime_int, size: u32) u32 {
+    switch (arity) {
+        3 => {
+            return calculate_segment_length_impl(size, 3.33, 2.25);
+        },
+        4 => {
+            return calculate_segment_length_impl(size, 2.91, -0.5);
+        },
+        else => {
+            @compileError("arity is not supported");
+        },
+    }
+}
+
+fn calculate_size_factor(comptime arity: comptime_int, size: u32) f64 {
+    const sz = @max(size, 2);
+
+    switch (arity) {
+        3 => {
+            return @max(1.125, 0.875 + 0.25 * @log(1000000.0) / @log(@as(f64, @floatFromInt(sz))));
+        },
+        4 => {
+            return @max(1.075, 0.77 + 0.305 * @log(600000.0) / @log(@as(f64, @floatFromInt(size))));
+        },
+        else => {
+            @compileError("arity is not supported");
+        },
+    }
+}
+
 pub fn construct(comptime Fingerprint: type, comptime arity: comptime_int, alloc: Allocator, hashes: []u64, seed: *u64, header: *Header) ConstructError![]Fingerprint {
-    const array_len: usize = @as(usize, @intFromFloat(32 + 1.23 * @as(f64, @floatFromInt(hashes.len))));
+    const size: u32 = @intCast(hashes.len);
+    const segment_length = @min(1 << 18, calculate_segment_length(arity, size));
+    const size_factor = calculate_size_factor(arity, size);
+    const capacity: u32 = @intFromFloat(@as(f64, @floatFromInt(size)) * size_factor);
+    const segment_count = (capacity + segment_length - 1) / segment_length - (arity - 1);
+    const array_len = (segment_count + arity - 1) * segment_length;
 
     var fingerprints = try alloc.alloc(Fingerprint, array_len);
     errdefer alloc.free(fingerprints);
@@ -98,11 +139,9 @@ pub fn construct(comptime Fingerprint: type, comptime arity: comptime_int, alloc
         @memset(set_xormask, 0);
         @memset(set_count, 0);
 
-        const block_len: u32 = @intCast(fingerprints.len / arity);
-
         for (hashes) |hash| {
             const h = hash ^ next_seed;
-            const subhashes = make_subhashes(arity, block_len, h);
+            const subhashes = make_subhashes(arity, header, h);
             for (subhashes) |subh| {
                 set_xormask[subh] ^= h;
                 set_count[subh] += 1;
@@ -121,7 +160,7 @@ pub fn construct(comptime Fingerprint: type, comptime arity: comptime_int, alloc
             const i = queue[queue_len];
             if (set_count[i] == 1) {
                 const h = set_xormask[i];
-                const subhashes = make_subhashes(arity, block_len, h);
+                const subhashes = make_subhashes(arity, header, h);
                 stack_h[stack_len] = h;
                 inline for (subhashes, 0..) |subh, hi| {
                     set_xormask[subh] ^= h;
@@ -145,7 +184,7 @@ pub fn construct(comptime Fingerprint: type, comptime arity: comptime_int, alloc
             stack_len -= 1;
             const h = stack_h[stack_len];
             const hi = stack_hi[stack_len];
-            const subhashes = make_subhashes(arity, block_len, h);
+            const subhashes = make_subhashes(arity, header, h);
             var f = make_fingerprint(Fingerprint, h);
             var to_change: u32 = undefined;
             inline for (subhashes, 0..) |subh, shi| {
@@ -176,11 +215,12 @@ test "smoke" {
     }
 
     var seed = rand.next();
-    const fingerprints = try construct(u16, 3, alloc, hashes, &seed);
+    var header: Header = undefined;
+    const fingerprints = try construct(u16, 3, alloc, hashes, &seed, &header);
     defer alloc.free(fingerprints);
 
     for (hashes) |h| {
-        try std.testing.expect(check(u16, 3, fingerprints, seed, h));
+        try std.testing.expect(check(u16, 3, &header, fingerprints, h));
     }
 }
 
@@ -202,31 +242,22 @@ fn to_fuzz(input: []const u8) anyerror!void {
     }
 
     var seed = rand.next();
-    const fingerprints = try construct(u8, 3, alloc, hashes, &seed);
+    var header: Header = undefined;
+    const fingerprints = try construct(u8, 3, alloc, hashes, &seed, &header);
     defer alloc.free(fingerprints);
 
     for (hashes) |h| {
-        try std.testing.expect(check(u8, 3, fingerprints, seed, h));
+        try std.testing.expect(check(u8, 3, &header, fingerprints, h));
     }
-
-    for (0..1000) |x| {
-        const h = std.hash.XxHash3.hash(rand.next(), std.mem.toBytes(x));
-        if (!check(u8, 3, fingerprints, seed, h)) {
-            try std.testing.expect(!contains(u64, hashes, h));
-        }
-    }
-}
-
-fn contains(comptime T: type, slice: []T, v: T) bool {
-    for (slice) |x| {
-        if (x == v) {
-            return true;
-        }
-    }
-
-    return false;
 }
 
 test "fuzz" {
     try std.testing.fuzz(to_fuzz, .{});
+}
+
+test "calculate segment length" {
+    const x = calculate_segment_length(3, 10000);
+    try std.testing.expect(x > 2 * 10000);
+    const factor = calculate_size_factor(3, 10000);
+    try std.testing.expect(factor == 0);
 }
