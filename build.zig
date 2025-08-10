8f369fdbb21cd48f47f3e918545cc0a7ee5511ed
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-08-11 01:10:45 +0600

    update to new zig version and bump package version

--- build.zig
+++ build.zig
@@ -13,10 +13,11 @@ pub fn build(b: *std.Build) void {
 
     const bench = b.addExecutable(.{
         .name = "bench",
-        .root_source_file = b.path("src/bench.zig"),
-        .target = target,
-        .optimize = optimize,
-        .use_llvm = true,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/bench.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
     bench.root_module.addImport("filterz", filterz_module);
 
@@ -32,9 +33,11 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/tests.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/tests.zig"),
+            .target = target,
+            .optimize = optimize,
+        }),
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
